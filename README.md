4fcd3b62ecdad76f7189513f208dac4d447236e8
inspiration2026 <inspiration.thailand@gmail.com>
2026-02-07 18:05:59 +0800

    Finish game interface and algorithms Add start screen with form Add input checks Add Scoreboard with custom names, live score and restart button Add start, restart, scoreAdjust functions into main object element Rewrite cleanField function Add IIFE function to start a game Add hover effect for play fields and buttons Make start and congratulation screens block gameplay Fill readme file

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# tic-tac-toe
+# Tic-Tac-Toe Game 🕹️
+A simple JavaScript game application where users can play a game "Tic-Tac-Toe" with AI Generated Cyberpunk style Symbols. 
+Educational project built while passing JavaScript course in Odin Project.
+
+For educational purposes project build without global variables. 
+
+# Features 👨‍💻
+Custom names on the scoreboard
+Display score during a game
+Congratulations screen
+Hover effects on buttons and playfield
+Clean styling in the dark tones
+Prevent marking playfield more than once
+Prevent hover effect on already selected fields
+
+# Method used ⌨️
+Factory functions
+IIFEs (Immediately Invoked Function Expressions)
+Flex & Grid
+
+# Author ✍️
+Roman Suslov (Learning JavaScript in 2025–2026)
 
 
-# brain-storm
+
+# initial-brain-storm 🧠
 - each field equal number 1 to 9 (better use f1-f9)
 - when player make a move - add his decision (variable) to his object
 - after each move check object for having any winning 
@@ -11,6 +33,5 @@ same value - if so - don't allow this move
 - the other way is eliminate variable from game array after each move and next move check if it's hasn't that variable - block a move.
 - if there is winning combination - finish the game and congrats the winner
 - if no variables left in game array and there are no winning combination - tie, start again
-
 - only after working console version - make HTML and CSS to interract with real user.
 
--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+
+
+
+
 (function() {
     const fields = document.querySelectorAll(".field");
-    const result = document.getElementById("result");
 
     fields.forEach (field => {
         field.addEventListener ("click", () => {
             const position = field.dataset.indexNumber;
-        if (!GameBoard.gameBoard.includes(position)) {return;}
+            if (!GameBoard.gameBoard.includes(position)) {return;}
             GameBoard.playMove(position);
-        if (checkWinner(GameBoard.playerOne)) {
-            console.log ("The winner is Player 1");
-            GameBoard.cleanField();
-            return;
-        } else if (checkWinner(GameBoard.playerTwo)) {
-            console.log ("The winner is Player 2");
-            GameBoard.cleanField();
-            return;
-        } else if (GameBoard.gameBoard.length === 0) {
-            alert ("It's a Tie");
-            GameBoard.cleanField();
-            return;
-        };
-        if (GameBoard.currentPlayer === "X") {
-            field.classList.add ("o")
-        } else if (GameBoard.currentPlayer === "O") {
-            field.classList.add ("x")
-        };    
+            if (GameBoard.currentPlayer === "X") {
+                field.classList.add ("o")
+            } else if (GameBoard.currentPlayer === "O") {
+                field.classList.add ("x")
+            };    
+            if (checkWinner(GameBoard.playerOne)) {
+                showWinScreen(GameBoard.playerOne.name, GameBoard.playerOne.symbol);
+                GameBoard.playerOne.score++;
+                GameBoard.scoreAdjust();
+                return;
+            } else if (checkWinner(GameBoard.playerTwo)) {
+                showWinScreen(GameBoard.playerTwo.name, GameBoard.playerTwo.symbol);
+                GameBoard.playerTwo.score++;
+                GameBoard.scoreAdjust();
+                return;
+            } else if (GameBoard.gameBoard.length === 0) {
+                showTieScreen();
+                return;
+            };
+        
         })
     })
 })();
@@ -52,11 +57,56 @@ const GameBoard = {
         });
         this.gameBoard = ["1","2","3","4","5","6","7","8","9"];
         this.currentPlayer = "X";
+        this.playerOne.field = [];
+        this.playerTwo.field = [];
+    },
+    scoreAdjust() {
+        const name1 = document.getElementById("name1");
+        const name2 = document.getElementById("name2");
+        const score1 = document.getElementById("score1");
+        const score2 = document.getElementById("score2");
+        const restartBtn = document.getElementById("restart");
+
+        name1.textContent = this.playerOne.name;
+        name2.textContent = this.playerTwo.name;
+        score1.textContent = this.playerOne.score;
+        score2.textContent = this.playerTwo.score;
+
+        restartBtn.addEventListener ('click', function() {
+            GameBoard.restart();
+        })
+    },
+    start() {
+        const player1 = document.getElementById("playerOne");
+        const player2 = document.getElementById("playerTwo");
+        const gameStart = document.getElementById("gameStart");
+        const startForm = document.getElementById("startForm");
+
+        player1.value = "";
+        player2.value = "";
+        gameStart.classList.remove("hidden");
+        player1.focus();
+
+        startForm.addEventListener ('submit', e => {
+            e.preventDefault();
+            gameStart.classList.add("hidden");
+            GameBoard.playerOne.name = player1.value;
+            GameBoard.playerTwo.name = player2.value;
+            GameBoard.playerOne.score = 0;
+            GameBoard.playerTwo.score = 0;
+            GameBoard.scoreAdjust()
+        })
+    },
+    restart() {
+        this.cleanField();
         this.playerOne = {symbol: "X", field:[]};
         this.playerTwo = {symbol: "O", field:[]};
+        this.start();
     }
-}
-
+};
+    (function () {
+        GameBoard.start();
+    })();
 // function getChoice() {
 //     const choice = prompt("enter number");
 //     return choice;
@@ -94,3 +144,34 @@ function checkWinner (player) {
 //     }
 // }
 // gameConsole();
+
+const overlayContainer = document.getElementById("overlay-container");
+const winTitle = document.getElementById("win-title");
+const winMessage = document.getElementById("win-message");
+const playAgain = document.getElementById("play-again");
+
+function showWinScreen(name,symbol) {
+    winTitle.textContent   = "Congratulations!";
+    winMessage.textContent = `Player ${name} "${symbol}" wins!`;
+
+    overlayContainer.classList.remove('hidden');
+}
+
+function showTieScreen() {
+    winTitle.textContent   = "It's a Tie!";
+    winMessage.textContent = "No winner this time";
+
+    overlayContainer.classList.remove('hidden');
+}
+
+playAgain.addEventListener('click', () => {
+    overlayContainer.classList.add('hidden');
+    GameBoard.cleanField();
+});
+
+overlayContainer.addEventListener('click', e => {
+    if (e.target === overlayContainer) {
+        overlayContainer.classList.add('hidden');
+        GameBoard.cleanField();
+    }
+});
